c9ce73f14e827567f257d09200d5ead49b251376
Furkan <frkngnl000@gmail.com>
2025-07-29 23:00:46 +0300

    Initial Commit

---
+++ lernig/lernigApp.swift
@@ -0,0 +1,17 @@
+//
+//  lernigApp.swift
+//  lernig
+//
+//  Created by Furkan Gönel on 29.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct lernigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
